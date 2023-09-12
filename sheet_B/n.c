91d4cbecc846ceb5ed11912ebb814f3ae78fd251
mohamed belal <mohamedbelal045@gmail.com>
2023-09-12 15:51:44 +0300

    add sheetb n.c

--- sheet_B/n.c
+++ sheet_B/n.c
@@ -1,3 +1,26 @@
 /*
- *	 n
+ *	 N. Numbers Histogram
  */
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    char s;
+    int temp;
+    scanf("%c %d", &s, &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        for (j = 0; j < temp; j++)
+        {
+            printf("%c", s);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
