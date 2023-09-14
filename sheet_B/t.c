5aaa811b237b94ea09e69eaae133f399e27f671f
mohamed belal <mohamedbelal045@gmail.com>
2023-09-14 11:03:30 +0300

    add sheetb t.c

--- sheet_B/t.c
+++ sheet_B/t.c
@@ -1,3 +1,24 @@
 /*
  *	 T. Shape2
  */
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i, t, numOfSymbol = 1, j;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        for (i = 0; i < t; i++)
+            printf(" ");
+        for (j = 0; j < numOfSymbol; j++)
+            printf("*");
+
+        numOfSymbol += 2;
+        printf("\n");
+    }
+    return 0;
+}
