978c5dc1fcc6260ad0f711e30243bae8467216a7
mohamed belal <mohamedbelal045@gmail.com>
2023-09-14 10:55:44 +0300

    add sheetb s.c

--- sheet_B/s.c
+++ sheet_B/s.c
@@ -1,3 +1,38 @@
 /*
  *	 S. Sum of Consecutive Odd Numbers
  */
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y, i, sum = 0, t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+
+        scanf("%d %d", &x, &y);
+
+        if (x > y)
+        {
+            for (i = (y + 1); i < x; i++)
+            {
+                if (i % 2 != 0)
+                    sum += i;
+            }
+        }
+        else
+        {
+            for (i = (x + 1); i < y; i++)
+            {
+                if (i % 2 != 0)
+                    sum += i;
+            }
+        }
+
+        printf("%d\n", sum);
+        sum = 0;
+    }
+    return 0;
+}
