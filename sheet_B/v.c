5322c11af64fc39a5d42982a8ca9984f93237764
mohamed belal <mohamedbelal045@gmail.com>
2023-09-15 12:45:46 +0300

    add sheet B v.c

--- sheet_B/v.c
+++ sheet_B/v.c
@@ -1,3 +1,19 @@
 /*
  *	 V. PUM
  */
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n, x = 1;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d %d %d PUM\n", x, x + 1, x + 2);
+        x += 4;
+    }
+
+    return 0;
+}
