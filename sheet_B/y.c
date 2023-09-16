c19b7311ec8a8844f43fea5c938cb5a1f93a0435
mohamed belal <mohamedbelal045@gmail.com>
2023-09-16 23:53:15 +0300

    add sheet b Y.c z.c

--- sheet_B/y.c
+++ sheet_B/y.c
@@ -1,3 +1,25 @@
 /*
  *	 Y. Easy Fibonacci
  */
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int fib[n];
+    fib[0] = 0; // fib(1)
+    fib[1] = 1; // fib(2)
+
+    for (i = 2; i < n; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", fib[i]);
+    }
+    return 0;
+}
--- sheet_B/z.c
+++ sheet_B/z.c
@@ -1,3 +1,21 @@
 /*
  *	 Z. Three Numbers
  */
+
+#include <stdio.h>
+
+int main()
+{
+    int k, s, x, y;
+    int count = 0;
+    scanf("%d %d", &k, &s);
+
+    for (x = 0; x <= k; x++)
+
+        for (y = 0; y <= k; y++)
+            if (s - x - y >= 0 && s - x - y <= k)
+                count++;
+
+    printf("%d", count);
+    return 0;
+}
