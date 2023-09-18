dc4ebc41f56396e02bfca041531fbd7b50756884
mohamed belal <mohamedbelal045@gmail.com>
2023-09-18 12:16:06 +0300

    add sheet c d.c

---
+++ sheet_C/d.c
@@ -0,0 +1,28 @@
+/*
+ *  sheet (c)   =>    D. Positions in array
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+
+        if (arr[i] <= 10)
+        {
+            printf("A[%d] = %d\n", i, arr[i]);
+        }
+    }
+
+    return 0;
+}
---
+++ sheet_C/e.c
@@ -0,0 +1,3 @@
+/*
+ *  sheet (c)   =>    E. Lowest Number
+ */
