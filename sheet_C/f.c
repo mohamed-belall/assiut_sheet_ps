45ab8d894f3667710b2c880c051ca332c0f62e0c
mohamed belal <mohamedbelal045@gmail.com>
2023-09-18 12:29:11 +0300

    add sheet c f.c

--- sheet_C/f.c
+++ sheet_C/f.c
@@ -1,3 +1,23 @@
 /*
  *  sheet (c)   =>    F. Reversing
  */
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = i - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
