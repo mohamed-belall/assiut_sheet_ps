c9589e0dad0f71c065a1bc7e5e383e4b077d9060
mohamed belal <mohamedbelal045@gmail.com>
2023-09-18 12:26:37 +0300

    add sheet c E.c

--- sheet_C/e.c
+++ sheet_C/e.c
@@ -1,3 +1,32 @@
 /*
  *  sheet (c)   =>    E. Lowest Number
  */
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, lnum = 10000, lposition = 10000;
+
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+
+        if (arr[i] < lnum)
+        {
+            lposition = i;
+            lnum = arr[i];
+        }
+    }
+
+    printf("%d %d", lnum, lposition + 1);
+
+    return 0;
+}
