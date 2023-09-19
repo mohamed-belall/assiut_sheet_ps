e636a77cbc60cb960ab170bfd30f8f4c3ecb973c
mohamed belal <mohamedbelal045@gmail.com>
2023-09-19 16:30:03 +0300

    add sheet c =>  I.c

--- sheet_C/i.c
+++ sheet_C/i.c
@@ -1,3 +1,39 @@
 /*
  *  sheet (c)   =>    I. Smallest Pair
  */
+
+#include <stdio.h>
+
+int main()
+{
+    int t, n, i, j;
+    long long eq;
+
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        int arr[n];
+        long long min = 1e9;
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+
+        for (i = 1; i <= n; i++)
+        {
+            for (j = i + 1; j <= n; j++)
+            {
+                eq = arr[i - 1] + arr[j - 1] + j - i;
+
+                if (eq < min)
+                {
+                    min = eq;
+                }
+            }
+        }
+        printf("%lld\n", min);
+    }
+
+    return 0;
+}
