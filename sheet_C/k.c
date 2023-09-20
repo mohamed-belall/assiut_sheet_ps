65b2b0d148f719f1287eebc70a8fec9442e997cb
mohamed belal <mohamedbelal045@gmail.com>
2023-09-20 20:11:05 +0300

    sheet c => K.c

--- sheet_C/k.c
+++ sheet_C/k.c
@@ -1,3 +1,26 @@
 /*
  *  sheet (c)   =>   K. Sum Digits
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i, x;
+    long long sum = 0;
+
+    scanf("%d", &n);
+    char str[n];
+    scanf("%s", &str);
+
+    for (i = 0; i < n; i++)
+    {
+        x = str[i] - '0';
+        sum += x;
+    }
+
+    printf("%lld", sum);
+
+    return 0;
+}
