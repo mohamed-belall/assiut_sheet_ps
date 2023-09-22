15dea66f876cbcc22896d8a90c58947590f4fcc7
mohamed belal <mohamedbelal045@gmail.com>
2023-09-22 16:01:25 +0300

    add sheet c => O.c

--- sheet_C/o.c
+++ sheet_C/o.c
@@ -2,3 +2,23 @@
  *  sheet (c)   =>  O. Fibonacci
  */
 
+#include <stdio.h>
+
+int main()
+{
+    long long n;
+    int i;
+    scanf("%lld", &n);
+    long long fib[n];
+    fib[0] = 0; // fib(1)
+    fib[1] = 1; // fib(2)
+
+    for (i = 2; i < n; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    printf("%lld ", fib[n - 1]);
+
+    return 0;
+}
