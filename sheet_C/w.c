e7d7b9335cae3b90992eabbb35bd36bcb2760843
mohamed belal <mohamedbelal045@gmail.com>
2023-09-26 23:42:29 +0300

    add w.c sheet c

--- sheet_C/w.c
+++ sheet_C/w.c
@@ -1,3 +1,35 @@
 /*
  *  sheet (c)   => W. Mirror Array
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+
+    int n, m, temp;
+
+    scanf("%d %d", &n, &m);
+    long long arr[n][m];
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%lld", &arr[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = m - 1; j >= 0; j--)
+        {
+            printf("%lld ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
